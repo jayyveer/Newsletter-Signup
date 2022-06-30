8cffbbdad89195a0b046a76f2b3b9db6fdecf3e7
Jayveer Singh <30889815+jayyveer@users.noreply.github.com>
2022-06-30 19:38:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const https = require("https");
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+    var firstName = req.body.fname;
+    var lastName = req.body.lname;
+    var email = req.body.email;
+    console.log(firstName, lastName, email); 
+    //dataobject
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    var jsonData = JSON.stringify(data);
+
+    const url = " https://us8.api.mailchimp.com/3.0/lists/ca10006632";
+    const options = {
+        method: "POST",
+        auth: "jayyveer:40720a5801297f83ebc8a5f2bd532ea1-us8"
+    }
+
+    const request = https.request(url, options, function(response){
+        if(response.statusCode === 200)
+        {
+            res.sendFile(__dirname + "/success.html");
+        }
+        else
+        {
+            res.sendFile(__dirname + "/failure.html");
+        }
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    })
+    request.write(jsonData);
+    request.end();
+});
+
+app.listen(procews.env.PORT  || 3000,function(){
+    console.log("Server is running on port 3000");
+})
+
+//api key
+//40720a5801297f83ebc8a5f2bd532ea1-us8
+
+//list id
+//ca10006632
